ff701796f0f7b3a3c9c376e1416857e625fefcf8
Dmitry A. Ustalov <dmitry@eveel.ru>
2012-04-20 05:54:27 +0600

    Actualize the Myasorubka::AOT class

--- lib/myasorubka/aot.rb
+++ lib/myasorubka/aot.rb
@@ -1,13 +1,12 @@
 # encoding: utf-8
 
 require 'tempfile'
-require 'iconv'
 
 require 'myaso/msd/russian'
 
-require 'myasorubka/adapters/aot/mrd_file'
-require 'myasorubka/adapters/aot/tab_file'
-require 'myasorubka/adapters/aot/msd'
+require 'myasorubka/aot/mrd_file'
+require 'myasorubka/aot/tab_file'
+require 'myasorubka/aot/msd'
 
 # AOT dictionaries adapter.
 #
@@ -18,38 +17,25 @@ require 'myasorubka/adapters/aot/msd'
 class Myasorubka::AOT
   attr_reader :mrd, :tab, :language, :encoding
 
-  def initialize(configuration = {}) # :nodoc:
-    @encoding = configuration[:encoding]
-    @language = configuration[:language]
+  def initialize(config = {}) # :nodoc:
+    @encoding = config[:encoding]
+    @language = config[:language]
 
-    to_tempfile(configuration[:mrd]).tap do |mrd_file|
+    to_tempfile(config[:mrd]).tap do |mrd_file|
       @mrd = Myasorubka::AOT::MRDFile.new(mrd_file, language)
       mrd_file.close
     end
 
-    to_tempfile(configuration[:tab]).tap do |tab_file|
+    to_tempfile(config[:tab]).tap do |tab_file|
       @tab = Myasorubka::AOT::TabFile.new(tab_file, language)
       tab_file.close
     end
   end
 
   def run! db, logger # :nodoc:
-    # gramtab -> msds
-    tab.ancodes.each_with_index do |(ancode, attrs), msd_id|
-      pos, grammemes = attrs[:pos], attrs[:grammemes]
-      msd = Myasorubka::AOT::MSD.send(language, pos, grammemes)
-      msd_hash = {
-        'pos' => msd.pos.to_s
-      }
-      msd_hash['ancode'] = ancode if ancode
-
-      db.msds[msd_id] = Myasorubka::AOT::MSD.include_msd(msd_hash, msd)
-    end
-    logger.info "MSDs: done (#{db.msds.size})"
-
     # prefixes -> prefixes
     mrd.prefixes.each_with_index do |prefix, id|
-      db.prefixes[id] = { 'prefix' => prefix }
+      db.prefixes.set(id, 'prefix' => prefix)
     end
     logger.info "Prefixes: done (#{db.prefixes.size})"
 
@@ -57,15 +43,20 @@ class Myasorubka::AOT
     rule_id = 0
     mrd.rules.each_with_index do |rules, rule_set_id|
       rules.each do |suffix, ancode, prefix|
+        pos, grammemes = tab.ancodes[ancode].values_at(:pos, :grammemes)
+
         rule_hash = {
-          'msd_id' => tab.ancodes[ancode][:id],
+          'msd' => MSD.send(language, pos, grammemes).to_s,
           'rule_set_id' => rule_set_id
         }
-        rule_hash['prefix'] = prefix if prefix
-        rule_hash['suffix'] = suffix if suffix
 
-        db.rules[rule_id] = rule_hash
+        rule_hash.merge! 'prefix' => prefix if prefix
+        rule_hash.merge! 'suffix' => suffix if suffix
+
+        db.rules.set(rule_id, rule_hash)
+
         rule_id += 1
+
         if 0 == rule_id % 1_000
           logger.info "Rules: now (#{rule_id}): " \
                       "#{prefix}...#{suffix} => #{ancode}"
@@ -83,27 +74,27 @@ class Myasorubka::AOT
         'rule_set_id' => rule_set_id,
         'stem' => stem
       }
+
       if ancode_hash = tab.ancodes[ancode]
         stem_hash['msd_id'] = ancode_hash[:id]
       end
-      db.stems[stem_id] = stem_hash
+
+      db.stems.set(stem_id, stem_hash)
 
       db.rules.select_by_rule_set(rule_set_id).each do |rule_id|
-        db.words[word_id] = {
-          'stem_id' => stem_id,
-          'rule_id' => rule_id
-        }
+        db.words.set(word_id, 'stem_id' => stem_id,
+                              'rule_id' => rule_id)
 
         word_id += 1
 
         if 0 == word_id % 5_000
-          last_word = db.assemble(word_id - 1)
+          last_word = db.words.assemble(word_id - 1)
           logger.info "Words: now (#{word_id}): #{last_word}"
         end
       end
 
       if 0 == (stem_id + 1) % 1_000
-        last_stem = db.stems[stem_id]['stem']
+        last_stem = db.stems.find(stem_id)['stem']
         logger.info "Stems: now (#{stem_id + 1}): #{last_stem}"
       end
     end
@@ -115,10 +106,8 @@ class Myasorubka::AOT
     def to_tempfile(source_filename)
       Tempfile.new('myaso').tap do |temp|
         if encoding
-          Iconv.open('UTF-8', encoding) do |cd|
-            IO.foreach(source_filename).each do |line|
-              temp.puts cd.iconv(line)
-            end
+          IO.foreach(source_filename).each do |line|
+            temp.puts line.encode!('UTF-8', encoding)
           end
         else
           IO.foreach(source_filename).each do |line|
